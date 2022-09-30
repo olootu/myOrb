bda15cc37343793ab3fad0242308385494fbaac5
Ola Ogunyemi <olootu6831@yahoo.co.uk git config --global user.email olootu6831@yahoo.co.uk>
2022-09-30 21:33:17 +0100

    Added search filter functionality

--- src/app/components/snomed-ct/snomed-ct.component.ts
+++ src/app/components/snomed-ct/snomed-ct.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { MyorbService } from 'src/app/services/myorb.service';
-import { FormControl } from '@angular/forms';
-import { debounceTime } from 'rxjs/operators';
+import { FormControl, Validators } from '@angular/forms';
+import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-snomed-ct',
@@ -10,12 +11,22 @@ import { debounceTime } from 'rxjs/operators';
 })
 export class SnomedCtComponent implements OnInit {
   gridData: [] = [];
-  filterControl = new FormControl();
+  filterControl = new FormControl('', [Validators.minLength(3)]);
+  subscription: Subscription;
 
   constructor(private myOrb: MyorbService) { }
 
   ngOnInit() {
     this.getSnomedCT();
+    this.subscription = this.filterControl.valueChanges.pipe(
+      debounceTime(300),
+      distinctUntilChanged()
+    )
+      .subscribe(value => {
+        if (value.length > 2) {
+          this.getSnomedCT(value);
+        }
+      })
   }
 
   getSnomedCT(term = 'ultrasound head') {
@@ -23,5 +34,13 @@ export class SnomedCtComponent implements OnInit {
       .subscribe((response: any) => {
         this.gridData = response.items;
       });
+  }
+  get formfilterControl(): any {
+    return this.filterControl;
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+
   }
 }
